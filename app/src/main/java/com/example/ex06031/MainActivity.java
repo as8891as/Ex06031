fd593d81fbfb387afc1830f43f3acff4bd50b08b
as8891as <as8891@bs.amalnet.k12.il>
2025-01-07 18:34:25 +0200

    First segment completed

--- app/src/main/java/com/example/ex06031/MainActivity.java
+++ app/src/main/java/com/example/ex06031/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ex06031;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +11,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textV=findViewById(R.id.textV);
+    }
+
+    public void pressed(View view) {
+        textV.setVisibility(View.VISIBLE);
     }
 }
