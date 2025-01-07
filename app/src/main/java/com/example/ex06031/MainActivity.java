e7dd33e1320339ec80875d8cb157b529fc27466f
as8891as <as8891@bs.amalnet.k12.il>
2025-01-07 18:39:39 +0200

    Second segment completed

--- app/src/main/java/com/example/ex06031/MainActivity.java
+++ app/src/main/java/com/example/ex06031/MainActivity.java
@@ -12,15 +12,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     TextView textV;
+    String text;
+    int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textV=findViewById(R.id.textV);
+        counter=0;
     }
 
     public void pressed(View view) {
+        counter++;
+        text="This is a click number: "+counter;
+        textV.setText(text);
         textV.setVisibility(View.VISIBLE);
     }
 }
